dfdd78ee6474e5892a28cf20a2f1f37d8954bfcf
Trevor DePew <t.depewmusic@gmail.com>
2022-12-06 11:11:56 -0500

    got conditional formatting working on first array elements onClick

--- src/components/NodeGrid.jsx
+++ src/components/NodeGrid.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 
 const onOffSwitch = (target) => {
-    return !target.active
+    target.active = !target.active
+    document.getElementById(`${target.id}`).style.background = target.active?"green":"lightgrey"
+    console.log(target)
 }
 
 const NodeGrid = ({ grid }) => {
@@ -19,20 +21,16 @@ const NodeGrid = ({ grid }) => {
         })
     }
     rowPopulator()
-    console.log(noteGrid)
     return (
         <div className="gridDiv">
             {
                 noteGrid.map((row) => {
-                    return row.map((node) => {
-                        for (let i = 1; i < row.length - 1; i++) {
-                            node.id = i//somehow i need to make the node id equal to the original id plus the position on the arrayt so each one is individual, perhaps it can be the name of the note concated with the id?????
-                        }
+                    return row.map((node, i) => {
+                        node.id = `${node.note}` + i
                         return (
                             <button 
                             id = {node.id}
                             onClick={() => {onOffSwitch(node)}} 
-                            style={{background: node.active ? "green" : "lightgrey"}}
                             >{node.note}</button>
                         )
                     })
