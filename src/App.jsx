45786c0b562e2316d12948f6d820a895671ebd21
Trevor DePew <t.depewmusic@gmail.com>
2022-12-06 14:21:50 -0500

    got the timer working

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
   return (
     <>
 
-        {/* <Header /> */}
+        <Header />
         <div className="main">
           {/* <Controls /> */}
           <NodeGrid grid={grid} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,46 @@
+import { useState, useEffect } from 'react'
+
 const Header = () => {
+  const [seconds, setSeconds] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+
+  const play = () => {
+    setIsActive(true)
+  }
+
+  const pause = () => {
+    setIsActive(false)
+  }
+
+  const reset = () => {
+    setSeconds(0);
+    setIsActive(false)
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
     return(
       <div className="header">
         <div className="title">
           <h1>Sequinzer 6001 </h1>
         </div>
+        <div className="speed">
+          {seconds}
+        </div>
         <div className = "transport">
-          <button className="play">Play</button>
-          <button className="pause">Pause</button>
-          <button className="stop">Stop</button>
+          <button onClick={play} className="play">Play</button>
+          <button onClick={pause} className="pause">Pause</button>
+          <button onClick={reset} className="stop">Stop</button>
         </div>
         <div className = "file">
           <button className="new">New</button>
