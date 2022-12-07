a5f5e8a2d5bd9c1f25de5b81b910e87935ca0453
Trevor DePew <t.depewmusic@gmail.com>
2022-12-06 22:50:53 -0500

    connected tempo slider to dial - setting up css positioning

--- src/components/Tempo.jsx
+++ src/components/Tempo.jsx
@@ -1,28 +1,40 @@
 import { useState, useEffect } from 'react'
 
-
 const Tempo = () => {
     const [rate, setRate] = useState(60)
     const slider = document.getElementById("tempo-control")
+    const radialDial = document.getElementById("tempo-dial")
     const slideHandler = () => {
         setRate(slider.value)
+        radialDial.style.transform = `rotate(${slider.value}deg)`
     }
     const tempo = rate;
 
     return (
         <div className="tempo">
             <p><label for="tempo-control">Tempo</label>
-            <input 
-                type="range" 
-                orient="vertical"
-                name="tempo-control"
-                id="tempo-control"
-                class="slider"
-                value={rate}
-                min="0" 
-                max="240"
-                onChange={slideHandler}
-            />
+            <div className="input">
+                <input 
+                    type="range" 
+                    orient="vertical"
+                    name="tempo-control"
+                    id="tempo-control"
+                    class="slider"
+                    value={rate}
+                    min="0" 
+                    max="280"
+                    onChange={slideHandler}
+                />
+                <div className="dial">
+                    <img 
+                        id="tempo-dial" 
+                        src="src/assets/radialDial.png" 
+                        height="50px" 
+                        width="50px"
+                    />
+                </div>
+
+            </div>
             <span class="slider_label">Tempo: {tempo}</span></p>
         </div>
     )
